438123bf1f08cb9999d2bacaa9ac43bf01f6aec0
Matthew Landauer <matthew@oaf.org.au>
2014-04-29 18:27:53 -0400

    Actually save

--- scraper.rb
+++ scraper.rb
@@ -15,20 +15,5 @@ page.search('.search-filter-results li').each do |li|
     member_for: li.search('dl dd')[0].inner_text,
     party: li.search('dl dd')[1].inner_text
   }
-  p record
+  ScraperWiki.save_sqlite(["name"], record)
 end
-
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
